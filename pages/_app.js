d3b75586a2e3827c424ef1c4d50997f6f04d1e8e
Peter Chen <peter@Peters-MacBook-Air.local>
2021-12-09 23:06:58 -0800

    added login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
+import { supabase } from '../utils/supabaseClient'
+import { Auth } from '@supabase/ui'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth.UserContextProvider supabaseClient={supabase}>
+	    <Component {...pageProps} />
+	  </Auth.UserContextProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,74 @@
-import Head from 'next/head'
+import React, { useEffect, useState } from "react";
+import { supabase } from '../utils/supabaseClient'
+import { Auth } from '@supabase/ui'
 
-export default function Home() {
+export default function IndexPage() {
+  const { user } = Auth.useUser()
+  const [isDesktop, setDesktop] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 900) {
+      setDesktop(true);
+    } else {
+      setDesktop(false);
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth > 900) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    window.addEventListener('resize', updateMedia);
+    return () => window.removeEventListener('resize', updateMedia);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    // bg-gray-700 min-h-screen min-w-screen
+    <div className="min-h-screen max-h-full min-w-screen flex flex-row justify-center">
+      {
+        isDesktop && (
+          <img src='https://wallpaper.dog/large/5574447.jpg' className=" object-cover md:w-1/2 xl:w-3/5"/>
+        )
+      }
+			
+      {/* 
+				* Check if user is logged in or not. 
+				* If not, display the login UI
+				* If logged in, display the app 
+				* & pass the user in as props
+				*/
+			!user ? (
+        <div className="container mx-auto max-w-2xl justify-center items-center p-4">
+          <div className="container mx-auto px-4 justify-start flex flex-col">
+            <div id="spacer" className="h-12"/>
+            <h1 className="text-black text-2xl font-black">Trading Simulation</h1>
+					  {/* This is the Login UI Component from SupabaseUI */}
+            <div className="bg-white rounded-lg py-4">
+            <Auth
+              supabaseClient={supabase}
+              socialLayout="horizontal"
+              socialButtonSize="xlarge"
+            />
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      ) : (
+        <div className="container mx-auto max-w-prose px-4">
+          <GratitudeApp user={user} />
+          <div id="spacer" className="h-12"/>
+          <button
+            className="text-pink-300 font-semibold"
+            onClick={async () => {
+              const { error } = await supabase.auth.signOut()
+              if (error) console.log('Error logging out:', error.message)
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   )
 }
